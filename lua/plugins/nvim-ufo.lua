950fdd6314294b9ef5544d64f7a7591eb9fd9477
Paul Im <mhim@daum.com>
2024-06-10 19:46:02 +0900

    modified ufo option

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,21 +1,64 @@
+---@diagnostic disable: missing-fields
 return {
-  {
-    "kevinhwang91/nvim-ufo",
-    event = "BufEnter",
-    dependencies = {
-      "kevinhwang91/promise-async",
-    },
-    config = function()
-      vim.o.foldcolumn = "1" -- '0' is not bad
-      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-      --- @diagnostic disable: unused-local
-      require("ufo").setup({
-        provider_selector = function(_bufnr, _filetype, _buftype)
-          return { "treesitter", "indent" }
-        end,
-      })
-    end,
+  "kevinhwang91/nvim-ufo",
+  event = "BufEnter",
+  dependencies = {
+    "kevinhwang91/promise-async",
   },
+
+  config = function()
+    vim.o.foldcolumn = "0" -- '0' is not bad
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+    vim.o.foldclose = "all"
+
+    local handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, { suffix, "MoreMsg" })
+      return newVirtText
+    end
+    -- Tell the server the capability of foldingRange,
+    -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+    local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+    for _, ls in ipairs(language_servers) do
+      require("lspconfig")[ls].setup({
+        capabilities = capabilities,
+        -- you can add other fields for setting up lsp server in this table
+      })
+    end
+    -- global handler
+    -- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+    -- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+    require("ufo").setup({
+      fold_virt_text_handler = handler,
+    })
+  end,
 }
